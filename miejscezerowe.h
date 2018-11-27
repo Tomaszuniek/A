3285d7845ed7f8de39e116fe0d7bb3e97bae3ac3
Tomaszuniek <gorka11@interia.eu>
2018-11-27 13:04:44 +0100

    Update miejscezerowe.h

--- miejscezerowe.h
+++ miejscezerowe.h
@@ -1,32 +1,46 @@
-
-#inlcude <klasakwadrat.h>
-
-double miejscezerowe(unsigned int ktore, myexception myex){
+double miejscezerowe(unsigned int ktore){
 
 double delta=(b*b)-(4*a*c);
 double m1;
 double m2;
+myexception myex;
+myex.setYourI(ktore);
+
 if(delta>0){
 
 	m1 =  ( (-b) +( pow(delta,0.5)) )  / (2*a);
 	m2 =  ( (-b) -( pow(delta,0.5)) )  / (2*a);
 
-	if(ktore==0){
-		return (m1<m2)?m1:m2;
+	if(ktore==1){
+		     return m1;
 	}
 
 	else{
-		return (m1>m2)?m1:m2;
+		if(ktore==2){
+		     return m2;
+		}
+		else{
+		     throw myex;
+		}
 	}
 
 }
 
 else if(delta==0){
-	return m1 = (-b)/(2*a);
+	if(ktore==1){
+	   return m1 = (-b)/(2*a);
+	}
+	else{
+	   throw myex;	
+	}
 }
 
 else{
-	 throw myex;	
+	 throw myex;
 }
 
 }
+};
+
+
+
