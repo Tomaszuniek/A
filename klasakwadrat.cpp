9fae2cce43b0fa2aadc05c454f02531ef0a41ce6
Tomaszuniek <gorka11@interia.eu>
2018-11-27 13:01:57 +0100

    Update klasakwadrat.cpp

--- klasakwadrat.cpp
+++ klasakwadrat.cpp
@@ -1,26 +1,37 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <string>
+#include <cstdio>
 using namespace std;
 
 
 class myexception: public exception
 {
+  public:
+  unsigned int i;
+  string ss;
+  void setYourI(unsigned int d){
+      this->i=d;
+      ss = "Nie ma miejsca " + to_string(i);
+  }
+
+
   virtual const char* what() const throw()
   {
-    return "Delta mniejsza od zera";
+    return ss.c_str();
   }
 };
 
 class Kwadrat{
 
 
-public:
+
 int a;
 int b;
 int c;
 
-
+public:
 Kwadrat(int aa, int bb, int cc){
 	a=aa;
 	b=bb;
@@ -34,32 +45,45 @@ Kwadrat(){
 }
 
 
-double miejscezerowe(unsigned int ktore, myexception myex){
+double miejscezerowe(unsigned int ktore){
 
 double delta=(b*b)-(4*a*c);
 double m1;
 double m2;
+myexception myex;
+myex.setYourI(ktore);
+
 if(delta>0){
 
 	m1 =  ( (-b) +( pow(delta,0.5)) )  / (2*a);
 	m2 =  ( (-b) -( pow(delta,0.5)) )  / (2*a);
 
-	if(ktore==0){
-		return (m1<m2)?m1:m2;
+	if(ktore==1){
+		     return m1;
 	}
 
 	else{
-		return (m1>m2)?m1:m2;
+		if(ktore==2){
+		     return m2;
+		}
+		else{
+		     throw myex;
+		}
 	}
 
 }
 
 else if(delta==0){
-	return m1 = (-b)/(2*a);
+	if(ktore==1){
+	   return m1 = (-b)/(2*a);
+	}
+	else{
+	   throw myex;	
+	}
 }
 
 else{
-	 throw myex;	
+	 throw myex;
 }
 
 }
@@ -69,17 +93,17 @@ else{
 
 int main(){
 
-myexception myex;
 
-Kwadrat kw(1,2,1);
+Kwadrat kw(1,10,1);
 
-unsigned int a=1;
+unsigned int a=3;
 
 
 try{
-cout<<kw.miejscezerowe(a, myex);
+cout<<kw.miejscezerowe(a);
 }catch(exception& e){
 cout<<e.what();
+
 }
 return 0;
 }
